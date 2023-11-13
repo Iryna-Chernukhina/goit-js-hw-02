d5dd4dd07e5901400b66d13a54d94318819742e9
Iryna-Chernukhina <130777228+Iryna-Chernukhina@users.noreply.github.com>
2023-11-13 19:34:23 +0100

    goit-js-hw-02

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,5 @@
 function checkForSpam(message) {
-    const lowerCaseMessage = message.toLowerCase();
-   if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
-        return true;
-  } else {
-        return false;
-  }
+  return message.toLowerCase().includes("spam") || message.toLowerCase().includes("sale");
 }
 
 
